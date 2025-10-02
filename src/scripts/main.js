64ca73808f921f3200c9a1f735c1bf8825e15f5c
chumachenkoUA <kirito22122005@gmail.com>
2025-10-02 03:00:40 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const toNumber = (str) => {
+  return Number(str.slice(1).split(',').join(''));
+};
+
+const sortList = (list) => {
+  const items = [...list.querySelectorAll('li')];
+
+  items.sort((a, b) => toNumber(b.dataset.salary) - toNumber(a.dataset.salary));
+
+  list.append(...items);
+};
+
+const getEmployees = (list) => {
+  return [...list.querySelectorAll('li')].map((el) => ({
+    name: el.textContent,
+    position: el.dataset.position,
+    salary: el.dataset.salary,
+    age: Number(el.dataset.age),
+  }));
+};
+
+const ul = document.querySelector('ul');
+
+sortList(ul);
